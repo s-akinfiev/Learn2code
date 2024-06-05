b824422a0cea2eb605e42fc79fd99c82cb1d21eb
Selusa Magda <selusama@oa-k6.msk.24-school.ru>
2024-06-05 22:46:54 +0300

    Исправил log_analyzer.sh добавил сортировку перед подсчетом uniq

--- src/log_analyzer.sh
+++ src/log_analyzer.sh
@@ -11,7 +11,7 @@ if [[ -f "$nf" && $nf =~ \.log$ ]]
 	#Порядок, в котором выводим статистику: количество уникальных строк, кол-во уникальный файлов, количество изменений (уникальные хэши - 1)
 	
 	c_lines=$(wc -l $nf | awk '{print $1}')
-	c_files=$(cat $nf | awk '{print $1}' | xargs basename | uniq | wc -l | awk '{print $1}')
+	c_files=$(cat $nf | awk '{print $1}' | xargs basename | sort | uniq | wc -l | awk '{print $1}')
 	c_chang=$(cat $nf | awk '{print $8}' | uniq | expr $(wc -l) - 1)
 	
 	echo "$c_lines $c_files $c_chang"
