57aa33f9d1f091b37ef416f134e99e416c534245
Selusa Magda <selusama@oa-k6.msk.24-school.ru>
2024-06-05 12:40:52 +0300

    Исправил скрипт edit.sh так, чтобы он принимал аргументы и писал абсолютные пути

--- src/edit.sh
+++ src/edit.sh
@@ -1,18 +1,26 @@
 #! /bin/bash
 
-echo "Какой файл?"
-read ptf
+#echo "Какой файл?"
+#read ptf
+
+ptf=$1
+quar=$2
+subs=$3
+
 nf=$(basename $ptf)
 echo $nf
+
 if [ -f "$nf" ]
+
 then
 echo "файл существует"
-echo "Что меняем?"
-read quar
-echo "На что меняем?"
-read subs
+#echo "Что меняем?"
+#read quar
+#echo "На что меняем?"
+#read subs
 sed -i '' 's/'$quar'/'$subs'/g' $nf
-echo "$ptf $( stat -f " - %z - %Sm" -t "%Y-%m-%d %H:%M" $nf) - $(shasum -a 256 $nf | awk '{print $1}') - sha256" >> files.log
+echo "$(realpath $nf) $( stat -f " - %z - %Sm" -t "%Y-%m-%d %H:%M" $nf) - $(shasum -a 256 $nf | awk '{print $1}') - sha256" >> files.log
+
 else
-echo "нет такого файла"
+echo "Нет такого файла"
 fi
