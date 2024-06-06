aac732bc0db126ef7224d3d05af4a2a6e4126ab9
Selusa Magda <selusama@oa-j3.msk.24-school.ru>
2024-06-06 18:29:59 +0300

    Выполнил quest5, сделал important_function.c

---
+++ src/important_function.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+double fun(double x);
+
+int main(void)
+{
+    double x = 0;
+    char term = ' ';
+    if (scanf("%lf%c", &x, &term) == 2 && term == '\n')
+    {
+        if (isnan(fun(x)) != 1)
+        { 
+            printf("%.1f", fun(x));
+        }
+        else
+        {
+            printf("n/a");
+        }
+    }
+    else
+    {
+        printf("n/a");
+    }
+    return 0;
+}
+
+double fun(double x)
+{ 
+    return (7e-3 * pow(x, 4) + ((22.8 * cbrt(x) - 1e3) * x + 3) / (x * x / 2) - x * pow((10. + x), (2. / x)) - 1.01);
+}
+
+/* y = 7e-3 * x^4 + ((22.8 * x^⅓ - 1e3) * x + 3) / (x * x / 2) - x * (10 + x)^(2/x) - 1.01. */
