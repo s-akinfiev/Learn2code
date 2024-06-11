5e6f8ff4d034eb71083720218d00cd898b52da5b
Selusa Magda <selusama@ox-u6.msk.21-school.ru>
2024-06-11 14:21:19 +0300

    Выполнил 3 задание, файл stat.c

--- src/stat.c
+++ src/stat.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #define NMAX 10
 
@@ -5,44 +6,29 @@ int input(int *a, int *n);
 void output(int *a, int n);
 int max(int *a, int n);
 int min(int *a, int n);
-/*double mean(int *a, int n);
+double mean(int *a, int n);
 double variance(int *a, int n);
-
-void output_result(int max_v,
-                   int min_v,
-                   double mean_v,
-                   double variance_v);
-*/
-int main()
-{
+void output_result(int max_v, int min_v, double mean_v, double variance_v);
+int main() {
     int n, data[NMAX];
-    if (input(data, &n) == 1){
-    output(data, n);
-    printf("\nmax is %d", max(data, n));
-    printf("\nmin is %d", min(data, n));
-    }
-/*  output_result(max(data, n),
-                  min(data, n),
-                  mean(data, n),
-                  variance(data, n));
-*/
-    else
-    printf("n/a");
+
+    if (input(data, &n) == 1) {
+        output(data, n);
+        output_result(max(data, n), min(data, n), mean(data, n), variance(data, n));
+    } else
+        printf("n/a");
     return 0;
 }
-
 
 int input(int *a, int *n) {
     char term = '\n';
 
     if (scanf("%d%c", n, &term) == 2 && *n > 0 && *n <= NMAX && term == '\n') {
         for (int *p = a; p - a < *n; p++) {
-            if ((scanf("%d%c", p, &term)) != 2 || !(term == '\n' || term == ' '))
-                return 0;
+            if ((scanf("%d%c", p, &term)) != 2 || !(term == '\n' || term == ' ')) return 0;
         }
-    }
-    else
-    return 0;
+    } else
+        return 0;
 
     return 1;
 }
@@ -52,7 +38,7 @@ void output(int *a, int n) {
         if (p - a != n - 1)
             printf("%d ", *p);
         else
-            printf("%d", *p);
+            printf("%d \n", *p);
     }
 }
 
@@ -60,8 +46,7 @@ int max(int *a, int n) {
     int result = *a;
 
     for (int *p = a; p - a < n; p++) {
-        if (*p > result)
-        result = *p;
+        if (*p > result) result = *p;
     }
     return result;
 }
@@ -70,10 +55,29 @@ int min(int *a, int n) {
     int result = *a;
 
     for (int *p = a; p - a < n; p++) {
-        if (*p < result)
-        result = *p;
+        if (*p < result) result = *p;
     }
     return result;
 }
 
+double mean(int *a, int n) {
+    double result = 0;
 
+    for (int *p = a; p - a < n; p++) {
+        result += *p;
+    }
+    return result / n;
+}
+
+double variance(int *a, int n) {
+    double mean_forall = mean(a, n);
+    double sq_sum = 0.;
+    for (int *p = a; p - a < n; p++) {
+        sq_sum += pow(*p - mean_forall, 2);
+    }
+    return sq_sum / n;
+}
+
+void output_result(int max_v, int min_v, double mean_v, double variance_v) {
+    printf("%d %d %.6f %.6f", max_v, min_v, mean_v, variance_v);
+}
