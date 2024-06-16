a93b27c2c7263142caf210cdbd45a1a4ac146e76
Ardell Lipia <ardellli@ox-e4.msk.21-school.ru>
2024-06-16 14:20:45 +0300

    Tolko vichislenie sosedey i zaciklennost

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#define height 80
+#define width 25
 
+int count_neighbours(int pole[width][height], int a, int b) {
+    int sosedi = 0;
+
+    for (int x = -1; x <= 1; x++) {
+        for (int y = -1, y < 1, y++) {
+            if (x != 0 || y != 0) {
+                    int ryad=(a+x+width)% width); //зацикленность по горизонтали
+                    int stolbik=(b+y+height)%height); //зацикленность по вертикали
+                    sosedi += pole[ryad][stolbik];
+            }
+        }
+
+        return sosedi;
+    }
