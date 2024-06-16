b1d1c945a04e2f526387740746f804b9684b209b
Selusa Magda <selusama@ox-j1.msk.21-school.ru>
2024-06-16 19:26:13 +0300

    Добавил проверку на окончание игры

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -3,20 +3,20 @@
 #define HEIGHT 25
 
 void render(int field[HEIGHT][WIDTH]);
+int end_game(int **generation, int **next_generation);
 
 void render(int field[HEIGHT][WIDTH]) {
     char cell_content = ' ';
-    field[HEIGHT][WIDTH] = 0;
-
+    
     printf("\e[1;1H\e[2J");
     for (int i = 0; i < WIDTH + 2; i++) printf("-");
     printf("\n");
     for (int y = 0; y < HEIGHT; y++) {
         printf("|");
         for (int x = 0; x < WIDTH; x++) {
-            if(field[y][x] == 1) cell_content = 'o';
+            if (field[y][x] == 1) cell_content = 'o';
             printf("%c", cell_content);
-            cell_content = ' ';    
+            cell_content = ' ';
         }
         printf("|");
         printf("\n");
@@ -25,17 +25,35 @@ void render(int field[HEIGHT][WIDTH]) {
     printf("\n");
 }
 
-int main(void) {
-    int field[HEIGHT][WIDTH];
+int end_game(int **generation, int **next_generation) {
+    int end = 0;
 
-    for (int y = 0; y < HEIGHT; y++)
+     for (int y = 0; y < HEIGHT; y++) 
         for (int x = 0; x < WIDTH; x++)
-            if ((x+ y) % 3 == 0)
-            field[y][x] = 1;
-            else
-            field[y][x] = 0;
+            if (generation[y][x] != 0)  end = 1;
+            
+    if (end != 1) {
+        for (int y = 0; y < HEIGHT; y++) 
+            for (int x = 0; x < WIDTH; x++)
+                if (generation[y][x] != next_generation[y][x]) end = 1;
+    }
+        
 
-    // while ()
-    render(field);
-    return 1;
-}
+        return end;
+    }
+
+    int main(void) {
+        int field[HEIGHT][WIDTH];
+
+        for (int y = 0; y < HEIGHT; y++)
+            for (int x = 0; x < WIDTH; x++)
+                if ((x + y) % 4 == 0)
+                    field[y][x] = 1;
+                else
+                    field[y][x] = 0;
+
+        while (end_game(generation, next_generation) != 0)
+        render(field);
+
+        return 1;
+    }
