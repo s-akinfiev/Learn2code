0f1cd609650e93e150b6eae6421fb3d3ae0c62e3
Ardell Lipia <ardellli@ox-e4.msk.21-school.ru>
2024-06-16 18:41:48 +0300

    Popitka 3

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -4,15 +4,34 @@
 
 int count_neighbours(int pole[width][height], int a, int b) {
     int sosedi = 0;
-
-    for (int x = -1; x <= 1; x++) {
-        for (int y = -1, y < 1, y++) {
-            if (x != 0 || y != 0) {
-                    int ryad=(a+x+width)% width); //зацикленность по горизонтали
-                    int stolbik=(b+y+height)%height); //зацикленность по вертикали
-                    sosedi += pole[ryad][stolbik];
+    
+    for (int x = a-1; x <= a+1; x++) {
+        for (int y = b-1; y < b+1; y++) {
+            if (x != a || y != b) {
+                int ryad=(a+x+width)% width; //зацикленность по горизонтали
+                int stolbik=(b+y+height)%height; //зацикленность по вертикали
+                sosedi += pole[ryad][stolbik];
             }
         }
-
+        
         return sosedi;
     }
+    
+    
+    int life_or_death(int pole[width] [height], int a,int b){
+int sosedi=count_neighbours(pole,a,b); 
+int life_flag;       
+        if (sosedi<2 || sosedi>3){
+            life_flag=0; // смерть клетки
+        }
+        else if (sosedi==3){
+            life_flag=1; //жизнь
+        }
+        else{
+            life_flag=0 // как было пусто
+        }
+        return life_flag
+        }
+    }
+   
+    
