2680ff5b40edfbeabc0377dd8ea47cddac67469b
Jenni Spot <jennispo@su-d3.msk.21-school.ru>
2024-06-16 23:01:27 +0300

    Done

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,30 +1,66 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define HEIGHT 25
 #define WIDTH 80
 
 int next_generation(int filed[HEIGHT][WIDTH]);
 int count_neighbours(int pole[HEIGHT][WIDTH], int a, int b);
-int life_or_death(int pole[HEIGHT] [WIDTH], int a,int b);
+int life_or_death(int pole[HEIGHT][WIDTH], int a, int b);
 void render(int field[HEIGHT][WIDTH]);
 int end_game(int generation[HEIGHT][WIDTH], int next_generation[HEIGHT][WIDTH]);
-
+int change_speed(int speed, int key);
+void get_initial(int field[HEIGHT][WIDTH]);
 
 int main() {
-    int field[HEIGHT][WIDTH];
+    int field[HEIGHT][WIDTH], life = 1, speed = 50000;
+    get_initial(field);
+    initscr();
+    start_color();
+    noecho();
+    keypad(stdscr, TRUE);
+    cbreak();
+    timeout(50);
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
 
-    for (int y = 0; y < HEIGHT; y++)
-        for (int x = 0; x < WIDTH; x++)
-            if ((x + y) % 4 == 0)
-                field[y][x] = 1;
-            else
-                field[y][x] = 0;
+    int key = 0;
 
-    
-    render(field);
+    while (life && key != 'q') {
+        key = getch();
+        speed = change_speed(speed, key);
+        render(field);
+        printw("Speed: %d\n", 11 - speed / 10000);
+        refresh();
+        usleep(speed);
+        life = next_generation(field);
+    }
+    if (!life) {
+        mvprintw(HEIGHT / 2, WIDTH / 2 - 2, "GAME END");
+        refresh();
+        getchar();
+    }
+
+    endwin();
 
     return 0;
+}
+
+void get_initial(int field[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            field[i][j] = 0;
+        }
+    }
+
+    int y, x;
+    while (scanf("%d %d%*[^\n]", &y, &x) == 2) {
+        field[y][x] = 1;
+        if (getchar() == 'q') break;
+    }
+    while (getchar() != '\n') {
+    }
 }
 
 int next_generation(int field[HEIGHT][WIDTH]) {
@@ -36,80 +72,83 @@ int next_generation(int field[HEIGHT][WIDTH]) {
         }
     }
 
-    field = temp;
+    int end = end_game(temp, field);
 
-    return 0;
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            field[y][x] = temp[y][x];
+        }
+    }
+
+    return end;
 }
-
-
-
 
 void render(int field[HEIGHT][WIDTH]) {
     char cell_content = ' ';
-    
-    printf("\e[1;1H\e[2J");
-    for (int i = 0; i < WIDTH + 2; i++) printf("-");
-    printf("\n");
+    clear();
+    for (int i = 0; i < WIDTH + 2; i++) printw("-");
+    printw("\n");
     for (int y = 0; y < HEIGHT; y++) {
-        printf("|");
+        printw("|");
         for (int x = 0; x < WIDTH; x++) {
             if (field[y][x] == 1) cell_content = 'o';
-            printf("%c", cell_content);
+            attron(COLOR_PAIR(1));
+            printw("%c", cell_content);
+            attroff(COLOR_PAIR(1));
             cell_content = ' ';
         }
-        printf("|");
-        printf("\n");
+        printw("|\n");
     }
-    for (int i = 0; i < WIDTH + 2; i++) printf("-");
-    printf("\n");
+    for (int i = 0; i < WIDTH + 2; i++) printw("-");
+    printw("\n");
 }
 
 int end_game(int generation[HEIGHT][WIDTH], int next_generation[HEIGHT][WIDTH]) {
     int end = 0;
 
-     for (int y = 0; y < HEIGHT; y++) 
+    for (int y = 0; y < HEIGHT; y++)
         for (int x = 0; x < WIDTH; x++)
-            if (generation[y][x] != 0)  end = 1;
-            
+            if (generation[y][x] != 0) end = 1;
+
     if (end != 1) {
-        for (int y = 0; y < HEIGHT; y++) 
+        for (int y = 0; y < HEIGHT; y++)
             for (int x = 0; x < WIDTH; x++)
                 if (generation[y][x] != next_generation[y][x]) end = 1;
     }
-        
 
-        return end;
-    }
-
-
-int count_neighbours(int pole[HEIGHT][WIDTH], int a, int b) {
-    int sosedi = 0;
-    
-    for (int x = a-1; x <= a+1; x++) {
-        for (int y = b-1; y < b+1; y++) {
-            if (x != a || y != b) {
-                int ryad=(a+x+WIDTH)% WIDTH; //зацикленность по горизонтали
-                int stolbik=(b+y+HEIGHT)%HEIGHT; //зацикленность по вертикали
-                sosedi += pole[ryad][stolbik];
-            }
-        }
-        
-        
-    }
-    return sosedi;
+    return end;
 }
 
-int life_or_death(int pole[HEIGHT] [WIDTH], int a,int b){
-int sosedi=count_neighbours(pole,a,b); 
-int life_flag;       
-        if (sosedi<2 || sosedi>3){
-            life_flag=0; // смерть клетки
+int count_neighbours(int field[HEIGHT][WIDTH], int y, int x) {
+    int neighbours = 0;
+    for (int i = y - 1; i <= y + 1; i++) {
+        for (int j = x - 1; j <= x + 1; j++) {
+            int row = i, col = j;
+            if (row < 0) row = HEIGHT + row;
+            row = row % HEIGHT;
+            if (col < 0) col = WIDTH + col;
+            col = col % WIDTH;
+            if ((i != y || j != x) && field[row][col]) neighbours++;
         }
-        else if (sosedi==3){
-            life_flag=1; //жизнь
+    }
+    return neighbours;
+}
+
+int life_or_death(int pole[HEIGHT][WIDTH], int a, int b) {
+    int sosedi = count_neighbours(pole, a, b);
+    int life_flag = 0;
+    if (pole[a][b]) {
+        if (sosedi >= 2 && sosedi <= 3) {
+            life_flag = 1;  // жизнь
         }
-        else{
-            life_flag=0; // как было пусто
-        }
-        return life_flag;
-        }
+    } else if (sosedi == 3) {
+        life_flag = 1;  // жизнь
+    }
+    return life_flag;
+}
+
+int change_speed(int speed, int key) {
+    if (key == 259 && speed > 10000) return speed - 10000;
+    if (key == 258 && speed < 100000) return speed + 10000;
+    return speed;
+}
