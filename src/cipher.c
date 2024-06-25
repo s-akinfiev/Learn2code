beabf82d6571b0a4e6791f1a4bebb11e967deb86
Selusa Magda <selusama@ox-k6.msk.21-school.ru>
2024-06-25 21:42:52 +0300

    quest2 done

--- src/cipher.c
+++ src/cipher.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
 int fileout(char *filename, char *filecontent);
+int fileappend(char *filename, char *filecontent);
 
 int main(void) {
     char file_name[100];
-    char file_content[256];
+    char file_content_read[256];
+    char file_content_append[256];
     int command;
 
     while (scanf("%d", &command) != 0 && command != -1) switch (command) {
             case 1:
-                scanf("%s", file_name);
-                if (fileout(file_name, file_content) != 0) {
+                scanf("%99s", file_name);
+                if (fileout(file_name, file_content_read) != 0) {
                 } else
                     printf("n/a\n");
                 break;
+            case 2:
+                getchar();
+                fgets(file_content_append, 256, stdin);
+                file_content_append[strlen(file_content_append) - 1] = '\0';
+                if (fileappend(file_name, file_content_append) != 0) {
+                    fileout(file_name, file_content_read);
+                } else
+                    printf("n/a\n");
+                break;
+
             default:
                 printf("n/a\n");
                 break;
@@ -25,14 +38,36 @@ int main(void) {
 
 int fileout(char *filename, char *filecontent) {
     int operaton_result = 0;
-    FILE *file = fopen(filename, "r");
-    if (file != NULL && filename && getc(file) != EOF) {
-        rewind(file);
-        while (fgets(filecontent, 256, file) != NULL) {
-            printf("%s\n", filecontent);
-        }
+    if (filename != NULL) {
+        FILE *file = fopen(filename, "r");
+        if (getc(file) != EOF) {
+            rewind(file);
+            while (fgets(filecontent, 256, file) != NULL) {
+                printf("%s\n", filecontent);
+            }
+            operaton_result = 1;
+        } else
+            operaton_result = 0;
+
         fclose(file);
-        operaton_result = 1;
+    } else
+        operaton_result = 0;
+    return operaton_result;
+}
+
+int fileappend(char *filename, char *filecontent) {
+    int operaton_result = 0;
+    if (filename != NULL) {
+        // char ch;
+        FILE *file = fopen(filename, "r+");
+        if (getc(file) != EOF) {
+            rewind(file);
+            fseek(file, 0, SEEK_END);
+            fputs(filecontent, file);
+            operaton_result = 1;
+        } else
+            operaton_result = 0;
+        fclose(file);
     } else
         operaton_result = 0;
 
